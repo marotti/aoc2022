e85870effd96e7a4cec9a5fc1829f052ea474137
josh@marotti.com <Gtu8o7t6f1!>
2022-12-03 00:47:10 -0500

    Day 3 Gotta love extension functions

--- src/Day03.kt
+++ src/Day03.kt
@@ -3,16 +3,20 @@ fun Char.getCharValue(): Int {
   return charList.indexOf(this) + 1
 }
 
+fun String.findCommonChar(vararg stringList: String): Char =
+  this.first { check -> stringList.all { check in it } }
+
+
 fun main() {
   fun part1(input: List<String>): Int =
     input
       .map { it.chunked(it.length / 2) }
-      .sumOf { (compartment1, compartment2) -> compartment1.toList().first { it in compartment2 }.getCharValue() }
+      .sumOf { (compartment1, compartment2) -> compartment1.findCommonChar(compartment2).getCharValue() }
 
   fun part2(input: List<String>): Int =
     input
       .chunked(3)
-      .sumOf { group -> group.first().first { check -> group.all { check in it } }.getCharValue() }
+      .sumOf { group -> group.first().findCommonChar(*group.toTypedArray()).getCharValue() }
 
   // test if implementation meets criteria from the description, like:
   val testInput = readInput("Day03_test")
