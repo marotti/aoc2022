3285f6abf9f815dffb6d4394441e911722f30dec
josh@marotti.com <Gtu8o7t6f1!>
2022-12-06 11:31:05 -0500

    Day 6 init.

---
+++ src/Day06.kt
@@ -0,0 +1,29 @@
+fun String.countOccurrenceOfEachChar(): Int =
+  this.fold(0) { acc, c -> acc + this.count { it == c } }
+
+fun String.getIndexOfNonrepeat(chunk: Int): List<Int> =
+  this.mapIndexed { index, _ ->
+    this.substring(index, minOf(index + chunk, this.length)).countOccurrenceOfEachChar()
+  }
+
+fun main() {
+  fun part1(input: List<String>): Int =
+    input.first().getIndexOfNonrepeat(4).indexOf(4) + 4
+
+
+  fun part2(input: List<String>): Int =
+    input.first().getIndexOfNonrepeat(14).indexOf(14) + 14
+
+// test if implementation meets criteria from the description, like:
+  val testInput = readInput("Day06_test")
+  val output1 = part1(testInput)
+  println(output1)
+  check(output1 == 7)
+  val output2 = part2(testInput)
+  println(output2)
+  check(output2 == 19)
+
+  val input = readInput("Day06")
+  println(part1(input))
+  println(part2(input))
+}
